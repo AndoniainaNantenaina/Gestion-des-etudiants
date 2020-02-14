4f38ebd68299f58a637e37c4a2f3dfd53976c0b9
AndoniainaNantenaina <49758977+AndoniainaNantenaina@users.noreply.github.com>
2020-02-14 12:59:29 +0300

    Add files via upload
    Projet Gestion des étudiants

---
+++ Form1.Designer.vb
@@ -0,0 +1,425 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Tabletxt = New System.Windows.Forms.TextBox()
+        Me.Mdptxt = New System.Windows.Forms.TextBox()
+        Me.nomUtilisateurtxt = New System.Windows.Forms.TextBox()
+        Me.porttxt = New System.Windows.Forms.TextBox()
+        Me.serveurtxt = New System.Windows.Forms.TextBox()
+        Me.Bdtxt = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.supptxt = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.AjoutEmailtxt = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.AjoutClassetxt = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.AjoutPrenomtxt = New System.Windows.Forms.TextBox()
+        Me.AjoutNomtxt = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Tabletxt)
+        Me.GroupBox1.Controls.Add(Me.Mdptxt)
+        Me.GroupBox1.Controls.Add(Me.nomUtilisateurtxt)
+        Me.GroupBox1.Controls.Add(Me.porttxt)
+        Me.GroupBox1.Controls.Add(Me.serveurtxt)
+        Me.GroupBox1.Controls.Add(Me.Bdtxt)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(622, 108)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Connection"
+        '
+        'Tabletxt
+        '
+        Me.Tabletxt.Location = New System.Drawing.Point(167, 32)
+        Me.Tabletxt.Name = "Tabletxt"
+        Me.Tabletxt.Size = New System.Drawing.Size(123, 20)
+        Me.Tabletxt.TabIndex = 12
+        '
+        'Mdptxt
+        '
+        Me.Mdptxt.Location = New System.Drawing.Point(483, 65)
+        Me.Mdptxt.Name = "Mdptxt"
+        Me.Mdptxt.Size = New System.Drawing.Size(123, 20)
+        Me.Mdptxt.TabIndex = 11
+        '
+        'nomUtilisateurtxt
+        '
+        Me.nomUtilisateurtxt.Location = New System.Drawing.Point(483, 32)
+        Me.nomUtilisateurtxt.Name = "nomUtilisateurtxt"
+        Me.nomUtilisateurtxt.Size = New System.Drawing.Size(123, 20)
+        Me.nomUtilisateurtxt.TabIndex = 10
+        '
+        'porttxt
+        '
+        Me.porttxt.Location = New System.Drawing.Point(167, 78)
+        Me.porttxt.Name = "porttxt"
+        Me.porttxt.Size = New System.Drawing.Size(123, 20)
+        Me.porttxt.TabIndex = 9
+        '
+        'serveurtxt
+        '
+        Me.serveurtxt.Location = New System.Drawing.Point(38, 78)
+        Me.serveurtxt.Name = "serveurtxt"
+        Me.serveurtxt.Size = New System.Drawing.Size(123, 20)
+        Me.serveurtxt.TabIndex = 8
+        Me.serveurtxt.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Bdtxt
+        '
+        Me.Bdtxt.Location = New System.Drawing.Point(38, 32)
+        Me.Bdtxt.Name = "Bdtxt"
+        Me.Bdtxt.Size = New System.Drawing.Size(123, 20)
+        Me.Bdtxt.TabIndex = 6
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(167, 16)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(34, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Table"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(398, 65)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(28, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Mdp"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(398, 32)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Utilisateur"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(167, 62)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(26, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Port"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Serveur"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Base de donnees"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(640, 48)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(123, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Se connecter"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(640, 74)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(123, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Actualiser"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.GroupBox4)
+        Me.GroupBox2.Controls.Add(Me.GroupBox3)
+        Me.GroupBox2.Controls.Add(Me.ListView1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 126)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(776, 471)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "BDD"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.supptxt)
+        Me.GroupBox4.Controls.Add(Me.Button4)
+        Me.GroupBox4.Location = New System.Drawing.Point(547, 315)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(200, 150)
+        Me.GroupBox4.TabIndex = 2
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Supprimer"
+        '
+        'supptxt
+        '
+        Me.supptxt.Location = New System.Drawing.Point(33, 45)
+        Me.supptxt.Name = "supptxt"
+        Me.supptxt.Size = New System.Drawing.Size(139, 20)
+        Me.supptxt.TabIndex = 1
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(68, 99)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = "Supprimer"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button3)
+        Me.GroupBox3.Controls.Add(Me.AjoutEmailtxt)
+        Me.GroupBox3.Controls.Add(Me.Label11)
+        Me.GroupBox3.Controls.Add(Me.AjoutClassetxt)
+        Me.GroupBox3.Controls.Add(Me.Label9)
+        Me.GroupBox3.Controls.Add(Me.AjoutPrenomtxt)
+        Me.GroupBox3.Controls.Add(Me.AjoutNomtxt)
+        Me.GroupBox3.Controls.Add(Me.Label8)
+        Me.GroupBox3.Controls.Add(Me.Label7)
+        Me.GroupBox3.Location = New System.Drawing.Point(6, 315)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(535, 150)
+        Me.GroupBox3.TabIndex = 1
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Ajouter"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(433, 42)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(96, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Ajouter"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'AjoutEmailtxt
+        '
+        Me.AjoutEmailtxt.Location = New System.Drawing.Point(327, 42)
+        Me.AjoutEmailtxt.Name = "AjoutEmailtxt"
+        Me.AjoutEmailtxt.Size = New System.Drawing.Size(96, 20)
+        Me.AjoutEmailtxt.TabIndex = 9
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(324, 26)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(32, 13)
+        Me.Label11.TabIndex = 8
+        Me.Label11.Text = "Email"
+        '
+        'AjoutClassetxt
+        '
+        Me.AjoutClassetxt.Location = New System.Drawing.Point(221, 42)
+        Me.AjoutClassetxt.Name = "AjoutClassetxt"
+        Me.AjoutClassetxt.Size = New System.Drawing.Size(100, 20)
+        Me.AjoutClassetxt.TabIndex = 5
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(218, 26)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(38, 13)
+        Me.Label9.TabIndex = 4
+        Me.Label9.Text = "Classe"
+        '
+        'AjoutPrenomtxt
+        '
+        Me.AjoutPrenomtxt.Location = New System.Drawing.Point(115, 42)
+        Me.AjoutPrenomtxt.Name = "AjoutPrenomtxt"
+        Me.AjoutPrenomtxt.Size = New System.Drawing.Size(100, 20)
+        Me.AjoutPrenomtxt.TabIndex = 3
+        '
+        'AjoutNomtxt
+        '
+        Me.AjoutNomtxt.Location = New System.Drawing.Point(9, 42)
+        Me.AjoutNomtxt.Name = "AjoutNomtxt"
+        Me.AjoutNomtxt.Size = New System.Drawing.Size(100, 20)
+        Me.AjoutNomtxt.TabIndex = 2
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(112, 26)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(43, 13)
+        Me.Label8.TabIndex = 1
+        Me.Label8.Text = "Prenom"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(6, 26)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(29, 13)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "Nom"
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader6})
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.Location = New System.Drawing.Point(6, 19)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(755, 290)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "ID"
+        Me.ColumnHeader1.Width = 34
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Nom"
+        Me.ColumnHeader2.Width = 141
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Prenom"
+        Me.ColumnHeader3.Width = 206
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Classe"
+        Me.ColumnHeader4.Width = 115
+        '
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Text = "Email"
+        Me.ColumnHeader6.Width = 251
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 609)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Bienvenue sur GDE"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Tabletxt As TextBox
+    Friend WithEvents Mdptxt As TextBox
+    Friend WithEvents nomUtilisateurtxt As TextBox
+    Friend WithEvents porttxt As TextBox
+    Friend WithEvents serveurtxt As TextBox
+    Friend WithEvents Bdtxt As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents AjoutPrenomtxt As TextBox
+    Friend WithEvents AjoutNomtxt As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents AjoutClassetxt As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents AjoutEmailtxt As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader4 As ColumnHeader
+    Friend WithEvents ColumnHeader6 As ColumnHeader
+    Friend WithEvents Button4 As Button
+    Friend WithEvents supptxt As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,103 @@
+﻿Imports MySql.Data
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim cn As MySqlConnection
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If Button1.Text = "Se connecter" Then
+
+            cn = New MySqlConnection("SERVER=" & serveurtxt.Text & ";PORT=" & Porttxt.Text & ";DATABASE=" & Bdtxt.Text & ";UID=" & nomUtilisateurtxt.Text & ";PWD=" & Mdptxt.Text)
+            Try
+                If cn.State = ConnectionState.Closed Then
+                    cn.Open()
+                    Button1.Text = "Se deconnecter"
+                End If
+            Catch ex As Exception
+
+            End Try
+        Else
+            cn.Close()
+            Button1.Text = "Se connecter"
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        If ConnectionState.Open Then
+
+            If AjoutNomtxt.Text.Trim() = "" Then
+                MessageBox.Show("Entrez un nom")
+            ElseIf AjoutPrenomtxt.Text.Trim() = "" Then
+                MessageBox.Show("Entrez un prenom")
+            ElseIf AjoutClassetxt.Text.Trim() = "" Then
+                MessageBox.Show("Entrez la classe")
+            ElseIf AjoutEmailtxt.Text.Trim() = "" Then
+                MessageBox.Show("Entrez un Email")
+            Else
+
+                Dim cmd As New MySqlCommand("INSERT INTO " & Tabletxt.Text & "(Nom,Prenom,Classe,Email) VALUES(@Nom,@Prenom,@Classe,@Email)", cn)
+                cmd.Parameters.AddWithValue("@Nom", AjoutNomtxt.Text)
+                cmd.Parameters.AddWithValue("@Prenom", AjoutPrenomtxt.Text)
+                cmd.Parameters.AddWithValue("@Classe", AjoutClassetxt.Text)
+                cmd.Parameters.AddWithValue("@Email", AjoutEmailtxt.Text)
+                cmd.ExecuteNonQuery()
+                cmd.Parameters.Clear()
+                AjoutNomtxt.Clear()
+                AjoutPrenomtxt.Clear()
+                AjoutClassetxt.Clear()
+                AjoutEmailtxt.Clear()
+                MessageBox.Show("Ajout termine")
+
+            End If
+        Else
+            MessageBox.Show("La connection n'est pas ouverte !", "FAILED")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If cn.State = ConnectionState.Open Then
+            ListView1.Items.Clear()
+
+            Dim cmd As New MySqlCommand("SELECT * FROM " & Tabletxt.Text, cn)
+            Using L As MySqlDataReader = cmd.ExecuteReader()
+
+                While L.Read()
+                    Dim ID As String = L("ID")
+                    Dim Nom As String = L("Nom")
+                    Dim Prenom As String = L("Prenom")
+                    Dim Classe As String = L("Classe")
+                    Dim Email As String = L("Email")
+
+                    ListView1.Items.Add(New ListViewItem(New String() {ID, Nom, Prenom, Classe, Email}))
+                End While
+
+            End Using
+        End If
+    End Sub
+
+    'Reste a faire (Suppression des donnees et modification)
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If ConnectionState.Open Then
+
+            If supptxt.Text.Trim() = "" Then
+                MessageBox.Show("Entrez un Nom")
+
+            Else
+
+                Dim cmd2 As New MySqlCommand("DELETE FROM " & Tabletxt.Text & "WHERE Nom=@Nom", cn)
+                cmd2.Parameters.AddWithValue("@Nom", supptxt.Text)
+                cmd2.ExecuteNonQuery()
+                cmd2.Parameters.Clear()
+                supptxt.Clear()
+                MessageBox.Show("Suppression termine")
+
+            End If
+        Else
+            MessageBox.Show("La connection n'est pas ouverte !", "FAILED")
+        End If
+    End Sub
+End Class
